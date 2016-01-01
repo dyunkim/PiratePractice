6c95a10ebac69cba565767ad405d70109521d0f7
dyunkim <dykim@smu.edu>
2016-01-01 16:22:32 -0600

    Added TomsHandwritten font to buttons

--- MainMenuScreen.java
+++ MainMenuScreen.java
@@ -2,10 +2,12 @@ package com.david.piratepractice;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.PerspectiveCamera;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
@@ -23,19 +25,17 @@ public class MainMenuScreen implements Screen{
     Stage stage;
     SpriteBatch batch;
     PiratePractice game;
-    private Camera camera;
 
     public MainMenuScreen(PiratePractice game) {
         this.game = game;
+        skinCreate();
         create();
     }
+
     public void create () {
-        camera = new PerspectiveCamera();
         batch = new SpriteBatch();
         stage = new Stage(new FitViewport(576, 1024));
         Gdx.input.setInputProcessor(stage);
-
-        skin = new Skin(Gdx.files.internal("skins/MenuSkin/uiskin.json"));
 
         Table table = new Table();
         table.setDebug(true);
@@ -50,12 +50,41 @@ public class MainMenuScreen implements Screen{
 
         Table buttonTable = new Table(skin);
         buttonTable.setBackground("button box");
+        buttonTable.setDebug(true);
         table.add(buttonTable);
 
         TextButton playButton = new TextButton("PLAY", skin, "Button");
+        playButton.addListener(new InputListener() {
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                System.out.println("Play");
+                return false;
+            }
+        });
         TextButton newButton = new TextButton("NEW GAME", skin, "Button");
+        newButton.addListener(new InputListener() {
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                System.out.println("New Game");
+                return false;
+            }
+        });
         TextButton settingsButton = new TextButton("SETTINGS", skin, "Button");
+        settingsButton.addListener(new InputListener() {
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                System.out.println("Settings");
+                return false;
+            }
+        });
         TextButton exitButton = new TextButton("EXIT", skin, "Button");
+        exitButton.addListener(new InputListener() {
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                System.out.println("Exit");
+                return false;
+            }
+        });
 
         buttonTable.top();
         buttonTable.add(playButton).padTop(64);
@@ -66,9 +95,6 @@ public class MainMenuScreen implements Screen{
         buttonTable.row();
         buttonTable.add(exitButton).padTop(64).padBottom(64);
 
-
-
-
     }
 
     public void show() {}
@@ -78,8 +104,6 @@ public class MainMenuScreen implements Screen{
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
         stage.draw();
-
-
     }
 
     @Override
@@ -106,6 +130,18 @@ public class MainMenuScreen implements Screen{
     public void dispose () {
         stage.dispose();
         skin.dispose();
+    }
+
+    private void skinCreate() {
+        skin = new Skin(Gdx.files.internal("skins/MenuSkin/uiskin.json"));
+        FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("skins/MenuSkin/tomshandwritten.ttf"));
+        FreeTypeFontGenerator.FreeTypeFontParameter parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
+        parameter.size = 35;
+        BitmapFont menuFont = generator.generateFont(parameter); // font size 12 pixels
+        skin.add("font", menuFont);
+        skin.get("Button", TextButton.TextButtonStyle.class).font = menuFont;
+
+        generator.dispose();
     }
 
 }
